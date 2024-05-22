53f9ce1f2f7691774f6ed6eaa6b4b64b899308b4
Huseyn Sharifzade <huseynsharif@gmail.com>
2024-05-22 23:25:37 +0400

    models

--- src/main/java/org/example/business/absracts/UserService.java
+++ src/main/java/org/example/business/absracts/UserService.java
@@ -1,7 +1,14 @@
 package org.example.business.absracts;
 
+import org.example.core.results.DataResult;
+import org.example.core.results.Result;
+import org.example.entities.User;
+import org.example.entities.dto.UserLoginRequest;
+import org.example.entities.dto.UserRegisterRequest;
+
 public interface UserService {
 
-
+    Result register(UserRegisterRequest user);
+    DataResult<String> login(UserLoginRequest user);
 
 }
